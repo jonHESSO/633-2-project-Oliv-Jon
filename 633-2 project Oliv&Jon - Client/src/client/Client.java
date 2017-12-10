bf6650c5c0ee0444f4d93d9bbafae5446979cf51
jonHESSO <jonathan.schnyder@students.hevs.ch>
2017-12-10 23:00:32 +0100

    Basic file list transfer completed
    The client gives the server a list of files, and the server returns a list of available files.

--- 633-2 project Oliv&Jon - Client/src/client/Client.java
+++ 633-2 project Oliv&Jon - Client/src/client/Client.java
@@ -1,15 +1,54 @@
-/**
+/*
  * 633-2 project Oliv&Jon - Client - Client.java
  * Author : Jonathan Schnyder
- * Created : 1 déc. 2017
+ * Created : 1 dÃ©c. 2017
  */
 
 package client;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 public class Client
 {
 	public static void main(String[] args)
 	{
-		
+		String serverName = "192.168.0.15" ;
+		InetAddress serverAddress ;
+		int port = 50000 ;
+		Socket clientSocket ;
+		ObjectInputStream inputStream ;
+		ObjectOutputStream outputStream ;
+		List<String> clientFiles = new ArrayList<>() ;
+		List<String[]> fileList = null ;
+		try {
+		    File directory = new File("data/") ;
+			File[] files = directory.listFiles() ;
+			System.out.println(files) ;
+            for (File f : files) {
+                clientFiles.add(f.getName()) ;
+            }
+			clientSocket = new Socket(InetAddress.getByName(serverName), port) ;
+			outputStream = new ObjectOutputStream(clientSocket.getOutputStream()) ;
+			outputStream.writeObject(clientFiles);
+			outputStream.flush();
+			inputStream = new ObjectInputStream(clientSocket.getInputStream()) ;
+			fileList = (ArrayList<String[]>)inputStream.readObject() ;
+			for (String[] s: fileList)
+			{
+				System.out.println(s[0]+" : "+s[1]) ;
+			}
+			clientSocket.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
--- 633-2 project Oliv&Jon - Server/src/server/Server.java
+++ 633-2 project Oliv&Jon - Server/src/server/Server.java
@@ -1,15 +1,60 @@
-/**
- * 633-2 project Oliv&Jon - Server - Server.java
- * Author : Jonathan Schnyder
- * Created : 1 déc. 2017
+/*
+  633-2 project Oliv&Jon - Server - Server.java
+  Author : Jonathan Schnyder
+  Created : 1 dÃ©c. 2017
  */
 
 package server;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 public class Server
 {
 	public static void main(String[] args)
 	{
-		
+		List fileList = new ArrayList<String[]>();
+		String serverName = "192.168.0.15" ;
+		Socket clientSocket = null ;
+		ServerSocket serverSocket ;
+		ObjectInputStream inputStream ;
+		ObjectOutputStream outputStream ;
+		int port = 50000 ;
+
+
+		try {
+			serverSocket = new ServerSocket(port, 10, InetAddress.getByName(serverName)) ;
+			clientSocket = serverSocket.accept() ;
+			InetAddress clientAddress = clientSocket.getInetAddress();
+			System.out.println("Client "+clientAddress+" has connected");
+			List<String> clientFiles ;
+			inputStream = new ObjectInputStream(clientSocket.getInputStream()) ;
+			clientFiles = (ArrayList<String>)inputStream.readObject();
+			for (String s : clientFiles)
+			{
+				String fileName = s ;
+				System.out.println(clientAddress+" : "+fileName) ;
+				String[] fileInfo = {""+clientAddress,fileName} ;
+				fileList.add(fileInfo) ;
+			}
+			outputStream = new ObjectOutputStream(clientSocket.getOutputStream()) ;
+			outputStream.writeObject(fileList);
+			outputStream.flush();
+			clientSocket.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+
+
+
+
 	}
 }
